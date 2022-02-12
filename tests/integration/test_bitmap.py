1601e905d7125d53b2de5e309ba4bfc8319fa948
spriggancg <spriggancg@gmail.com>
2022-02-12 10:07:47 +0100

    add mnist test simple bitmap rendering

--- tests/integration/test_bitmap.py
+++ tests/integration/test_bitmap.py
@@ -10,12 +10,14 @@ def test_render_bitmap_from_csv():
 
     # define test case
     input_path = os.path.dirname(os.path.realpath(__file__)) \
-        + "/../fixtures/titanic-dataset-train.csv"
+        + "/../fixtures/mnist_test.csv"
     output_path = os.path.dirname(
-        os.path.realpath(__file__)) + "/output-bitmap.png"
+        os.path.realpath(__file__)) + "/output_bitmap_mnist.png"
 
     # Instanciate Hishiryo
     HishiryoConverter = Hishiryo.Hishiryo()
     assert HishiryoConverter.convertCSVToBitmap(input_path,
                                                 output_path,
                                                 )
+
+
