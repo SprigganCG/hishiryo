78377c6a3bc41795d1048d62f6ceebb73b034438
spriggancg <spriggancg@gmail.com>
2022-02-12 10:12:16 +0100

    run black on tests

--- tests/integration/test_bitmap.py
+++ tests/integration/test_bitmap.py
@@ -4,20 +4,20 @@ from hishiryo import Hishiryo
 import os
 
 
-# simple run against a test csv dataset and generate a
-# simple bitmap
 def test_render_bitmap_from_csv():
+    """Render a simple square bitmap from a csv"""
 
     # define test case
-    input_path = os.path.dirname(os.path.realpath(__file__)) \
-        + "/../fixtures/mnist_test.csv"
-    output_path = os.path.dirname(
-        os.path.realpath(__file__)) + "/output_bitmap_mnist.png"
+    input_path = (
+        os.path.dirname(os.path.realpath(__file__)) + "/../fixtures/mnist_test.csv"
+    )
+    output_path = (
+        os.path.dirname(os.path.realpath(__file__)) + "/output_bitmap_mnist.png"
+    )
 
     # Instanciate Hishiryo
     HishiryoConverter = Hishiryo.Hishiryo()
-    assert HishiryoConverter.convertCSVToBitmap(input_path,
-                                                output_path,
-                                                )
-
-
+    assert HishiryoConverter.convertCSVToBitmap(
+        input_path,
+        output_path,
+    )
--- tests/integration/test_radialbitmap_glyphtype.py
+++ tests/integration/test_radialbitmap_glyphtype.py
@@ -4,14 +4,15 @@ from hishiryo import Hishiryo
 import os
 
 
-# simple run against a test csv dataset to generate dot bitmap
 def test_render_dot_bitmap_from_csv():
+    """Simple run against a test csv dataset to generate dot bitmap"""
 
     # define test case
-    input_path = os.path.dirname(os.path.realpath(
-        __file__)) + "/../fixtures/titanic-dataset-train.csv"
-    output_path = os.path.dirname(
-        os.path.realpath(__file__)) + "/output-dot.png"
+    input_path = (
+        os.path.dirname(os.path.realpath(__file__))
+        + "/../fixtures/titanic-dataset-train.csv"
+    )
+    output_path = os.path.dirname(os.path.realpath(__file__)) + "/output-dot.png"
     separator = ","
     radius = 3000
     sort_by = "PassengerId"
@@ -19,23 +20,19 @@ def test_render_dot_bitmap_from_csv():
 
     # Instanciate Hishiryo
     HishiryoConverter = Hishiryo.Hishiryo()
-    assert HishiryoConverter.convertCSVToRadialBitmap(input_path,
-                                                      separator,
-                                                      output_path,
-                                                      radius,
-                                                      sort_by,
-                                                      glyph_type
-                                                      )
+    assert HishiryoConverter.convertCSVToRadialBitmap(
+        input_path, separator, output_path, radius, sort_by, glyph_type
+    )
 
 
-# simple run against a test csv dataset to generate square bitmap
 def test_render_square_bitmap_from_csv():
-
+    """Simple run against a test csv dataset to generate square bitmap"""
     # define test case
-    input_path = os.path.dirname(os.path.realpath(
-        __file__)) + "/../fixtures/titanic-dataset-train.csv"
-    output_path = os.path.dirname(
-        os.path.realpath(__file__)) + "/output-square.png"
+    input_path = (
+        os.path.dirname(os.path.realpath(__file__))
+        + "/../fixtures/titanic-dataset-train.csv"
+    )
+    output_path = os.path.dirname(os.path.realpath(__file__)) + "/output-square.png"
     separator = ","
     radius = 3000
     sort_by = "PassengerId"
@@ -43,23 +40,20 @@ def test_render_square_bitmap_from_csv():
 
     # Instanciate Hishiryo
     HishiryoConverter = Hishiryo.Hishiryo()
-    assert HishiryoConverter.convertCSVToRadialBitmap(input_path,
-                                                      separator,
-                                                      output_path,
-                                                      radius,
-                                                      sort_by,
-                                                      glyph_type
-                                                      )
+    assert HishiryoConverter.convertCSVToRadialBitmap(
+        input_path, separator, output_path, radius, sort_by, glyph_type
+    )
 
 
-# simple run against a test csv dataset to generate polygon bitmap
 def test_render_polygon_bitmap_from_csv():
+    """Simple run against a test csv dataset to generate polygon bitmap"""
 
     # define test case
-    input_path = os.path.dirname(os.path.realpath(
-        __file__)) + "/../fixtures/titanic-dataset-train.csv"
-    output_path = os.path.dirname(
-        os.path.realpath(__file__)) + "/output-polygon.png"
+    input_path = (
+        os.path.dirname(os.path.realpath(__file__))
+        + "/../fixtures/titanic-dataset-train.csv"
+    )
+    output_path = os.path.dirname(os.path.realpath(__file__)) + "/output-polygon.png"
     separator = ","
     radius = 3000
     sort_by = "PassengerId"
@@ -67,10 +61,6 @@ def test_render_polygon_bitmap_from_csv():
 
     # Instanciate Hishiryo
     HishiryoConverter = Hishiryo.Hishiryo()
-    assert HishiryoConverter.convertCSVToRadialBitmap(input_path,
-                                                      separator,
-                                                      output_path,
-                                                      radius,
-                                                      sort_by,
-                                                      glyph_type
-                                                      )
+    assert HishiryoConverter.convertCSVToRadialBitmap(
+        input_path, separator, output_path, radius, sort_by, glyph_type
+    )
