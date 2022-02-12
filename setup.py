541533d701101b9b87fbcafaa5451611f3200fe7
spriggancg <spriggancg@gmail.com>
2022-02-12 16:25:08 +0100

    adjust pandas max version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    install_requires=['pandas<=1.3.0', 'opencv-python'],
+    install_requires=['pandas<=1.3.5', 'opencv-python'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
